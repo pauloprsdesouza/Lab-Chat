e5b26ca52d72233c7052e20bedf6eb83a7fdfe55
Paulo Roberto de Souza <paulo.prsdesouza@gmail.com>
2021-11-11 21:02:24 -0300

    add cloudformation configuration

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -2,13 +2,25 @@
 
 set -e
 
+basedir=$(cd $(dirname $0) ; pwd)/..
+
+cd $basedir/src
+
+rm -rf $basedir/dist
+mkdir $basedir/dist
+
+cp $basedir/src/CloudFormation.yaml $basedir/dist
+
+dotnet publish -c release -o $basedir/dist
+
 aws cloudformation package \
-  --template-file ./src/CloudFormation.yaml \
-  --output-template-file ./dist/deploy.yaml \
+  --template-file $basedir/dist/CloudFormation.yaml \
+  --output-template-file $basedir/dist/deploy.yaml \
   --s3-bucket pauloprsdesouza-dev \
-  --s3-prefix cloud-formation/lab.chat
+  --s3-prefix cloud-formation/lab-chat
 
 aws cloudformation deploy \
-  --stack-name lab.chat \
-  --template-file ./dist/deploy.yaml \
+  --stack-name lab-chat \
+  --template-file $basedir/dist/deploy.yaml \
   --parameter-overrides Environment=Development \
+  --capabilities CAPABILITY_IAM \
